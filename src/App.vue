c6b296a44b31a37b69952baee6154f15f1a797d1
王栋栋 <DQTWDD@dongyudeMacBook-Pro.local>
2019-03-23 21:57:37 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,255 @@
+<template>
+  <div>
+    <div>
+      <span class = 'spanline'></span>
+      <h1>购物车</h1>
+    </div>
+    <div class = 'table'>
+      <div class = 'tabletitle'>
+        <p class = 'op'>商品信息</p>
+        <p class = 'tp'>商品金额</p>
+        <p class = 'thp'>商品数量</p>
+        <p class = 'thp'>总金额</p>
+        <p class = 'thp'>编辑</p>
+      </div>
+      <div>
+        <ul>
+          <listshow
+          v-for= '(item,index) of list' :key = index
+          :item = 'item'
+          :list = 'list'
+          :msg = 'msg'
+          >
+          </listshow>
+        </ul>
+      </div>
+      <div class = 'tablebottom'>
+        <input class = 'checkbox' type = 'checkbox' :checked = 'checkall' @click = 'ifcheckall () , calctotalmoney ()'>
+        <div class = 'selectAll' @click = 'mcheckall ()'>全选</div>
+        <div class = 'noselectAll' @click = 'mnocheckall()'>取消全选</div>
+        <div class = 'sumPrice'>{{totalmoney}}</div>
+        <button>结算</button>
+      </div>
+    </div>
+    <!-- <img :src = 'list[0].imgPic'> -->
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+import showlist from './components/showlist'
+export default {
+  components: {
+    'listshow': showlist
+  },
+  data () {
+    return {
+      msg: 'helloworld',
+      checkall: false,
+      item: '',
+      totalmoney: 0,
+      list: [
+        {
+          'name': '面具1',
+          'checked': false,
+          'price': 19,
+          'count': 1,
+          'amount': 19,
+          'imgPic': '../static/img/test1.jpg',
+          'gifts': [
+            {
+              'partId': '10001',
+              'partName': '火'
+            },
+            {
+              'partId': '10002',
+              'partName': '火柴'
+            }
+          ]
+        },
+        {
+          'name': '面具2',
+          'checked': false,
+          'price': 20,
+          'count': 1,
+          'amount': 20,
+          'imgPic': '../static/img/test.jpg',
+          'gifts': [
+            {
+              'partId': '10001',
+              'partName': '打'
+            },
+            {
+              'partId': '10002',
+              'partName': '火柴盒'
+            }
+          ]
+        },
+        {
+          'name': '面具3',
+          'checked': false,
+          'price': 21,
+          'count': 1,
+          'amount': 20,
+          'imgPic': '../static/img/goods-1.jpg',
+          'gifts': [
+            {
+              'partId': '10001',
+              'partName': '机'
+            },
+            {
+              'partId': '10002',
+              'partName': '火柴盒'
+            }
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
+    ifcheckall: function () {
+      this.checkall = !this.checkall
+      this.list.forEach((item) => {
+        item.checked = this.checkall
+        console.log(this.checkall)
+      })
+      // if (this.checkall) {
+      //   this.list.forEach(function (item, checkall) {
+      //     item.checked = true
+      //   })
+      // } else {
+      //   this.list.forEach(function (item, checkall) {
+      //     item.checked = false
+      //   })
+      // }
+      // console.log(checked)
+    },
+    mcheckall: function () {
+      this.checkall = true
+      this.list.forEach(function (item) {
+        item.checked = true
+      })
+      this.calctotalmoney()
+    },
+    mnocheckall: function () {
+      this.checkall = false
+      this.list.forEach(function (item) {
+        item.checked = false
+      })
+      this.calctotalmoney()
+    },
+    calctotalmoney: function () {
+      var _this = this
+      _this.totalmoney = 0
+      this.list.forEach(function (item) {
+        if (item.checked) {
+          _this.totalmoney += item.count * item.price
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  border: 0px;
+  margin:0px;
+  padding: 0px;
+}
+#app {
+
+}
+h1 {
+  padding: 20px 20px;
+  color: #605f5f;
+  text-align: center;
+  z-index: 10;
+  background-color: white;
+}
+.spanline {
+  width: 1200px;
+  position: absolute;
+  left: 50%;
+  top: 40px;
+  margin-left: -600px;
+  border-bottom: 1px solid #605f5f;
+}
+.table {
+  width: 1200px;
+  border: 1px solid #605f5f;
+  position: absolute;
+  top: 100px;
+  left: 50%;
+  margin-left: -600px;
+}
+.tabletitle {
+  height: 50px;
+  color: white;
+  background-color: #605f5f;
+}
+.tabletitle p {
+  position: relative;
+  display: inline;
+  top: 10px;
+  font-size: 20px;
+}
+.op {
+  margin-left: 220px;
+}
+.tp {
+  margin-left: 230px;
+}
+.thp {
+  margin-left: 100px;
+}
+.tablebottom {
+  width: 1200px;
+  position: relative;
+  left: 50%;
+  bottom: -80px;
+  margin-left: -600px;
+}
+.tablebottom input {
+  position: relative;
+  top: 20px;
+  height: 20px;
+  width: 20px;
+  margin-right: 20px;
+}
+.selectAll {
+  position: relative;
+  top: 20px;
+  display: inline-block;
+  font-size: 20px;
+}
+.noselectAll {
+  position: relative;
+  top: 20px;
+  display: inline-block;
+  font-size: 20px;
+}
+.tablebottom button {
+  position: relative;
+  top: 20px;
+  left: 740px;
+  display: inline-block;
+  width: 220px;
+  height: 55px;
+  color: white;
+  font-size: 20px;
+  background-color:#E43537;
+}
+.sumPrice {
+  position: relative;
+  top: 20px;
+  left: 750px;
+  width: 60px;
+  display: inline-block;
+  color: #e4393c;
+  font-size: 20px;
+}
+.checkbox {
+  border-radius: 50%
+}
+</style>
---
+++ src/components/showlist.vue
@@ -0,0 +1,135 @@
+<template>
+<div class = 'tobuy'>
+  <li>
+    <input class = 'select' type = 'radio' :checked = item.checked @click = 'itemcheck'>
+    <img :src = 'item.imgPic' class = 'itemimg'>
+    <div class = 'itemname'>{{item.name}}</div>
+    <div class = 'free'>
+    赠品:
+    <p>{{item.gifts[0].partName}}</p>
+    </div>
+    <div class = 'price'>{{item.price}}</div>
+    <button class = 'buttonm' @click = 'numm'>-</button>
+    <input class = 'number' v-model = item.count>
+    <button class = 'buttonp' @click = 'nump'>+</button>
+    <div class = 'sum' >{{item.count * item.price}}</div>
+    <img :src = 'deleteicon' class = 'dicon'>
+  </li>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['item', 'index'],
+  data () {
+    return {
+      inputValue: '',
+      deleteicon: '../../static/img/delete.png',
+      sumprice: ''
+    }
+  },
+  methods: {
+    itemcheck: function () {
+      this.item.checked = !this.item.checked
+      this.$parent.calctotalmoney()
+    },
+    nump: function () {
+      this.item.count++
+      this.$parent.calctotalmoney()
+    },
+    numm: function () {
+      if (this.item.count > 1) {
+        this.item.count--
+        this.$parent.calctotalmoney()
+      } else {
+        this.item.count = 1
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+li {
+    list-style-type: none;
+    height: 130px;
+    width: 1200px;
+    overflow: hidden;
+    margin-top: 20px;
+    margin-bottom: -60px;
+
+}
+.select{
+    position: relative;
+    top: 10px;
+    left: 50px;
+}
+.itemimg {
+    position: relative;
+    top: 30px;
+    left: 100px;
+    height: 60px;
+    width: 60px;
+}
+.itemname{
+    position: relative;
+    top: -40px;
+    left: 200px;
+}
+.free {
+    position: relative;
+    top: -23px;
+    left: 200px;
+    overflow: hidden;
+}
+.free p {
+    color: #ccc;
+    position: relative;
+    left: 50px;
+    top: -22px;
+}
+.number {
+    position: relative;
+    height: 25px;
+    width: 80px;
+    border: 1px solid #ccc;
+    left: 695px;
+    top: -115px;
+}
+.buttonm {
+    position: relative;
+    height: 18px;
+    width: 18px;
+    left: 680px;
+    top: -115px;
+}
+.buttonp {
+    position: relative;
+    height: 18px;
+    width: 18px;
+    left: 700px;
+    top: -115px;
+}
+.price {
+    color:#e4393c;
+    position: relative;
+    height: 25px;
+    left: 565px;
+    top: -90px;
+}
+.sum {
+    position: relative;
+    top: -140px;
+    left: 910px;
+    color: #ee4433
+}
+.dicon {
+    position: relative;
+    top: -170px;
+    left: 1070px;
+    height: 40px;
+    width: 40px;
+}
+
+</style>
